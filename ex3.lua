d3c3fd689b9a0e62d2ec5f2da7af74160ef9cc0c
HabibAroua <habibha.aroua82@gmail.com>
2019-10-29 10:30:04 +0100

    add new file

---
+++ ex3.lua
@@ -0,0 +1,8 @@
+require 'paths'
+if (not paths.filep("cifar10torchsmall.zip")) then
+    os.execute('wget -c https://s3.amazonaws.com/torch7/data/cifar10torchsmall.zip')
+    os.execute('unzip cifar10torchsmall.zip')
+end
+trainset = torch.load('cifar10-train.t7')
+testset = torch.load('cifar10-test.t7')
+classes = {'airplane', 'automobile', 'bird', 'cat','deer', 'dog', 'frog', 'horse', 'ship', 'truck'}
